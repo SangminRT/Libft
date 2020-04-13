52da70b3b71d0452c27912123a8b07242cfab00f
한상민 <javis@hansangmins-Mac.local>
2020-04-13 10:54:07 +0900

    sm

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sanhan <sanhan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 06:24:55 by sanhan            #+#    #+#             */
-/*   Updated: 2020/04/06 09:21:52 by sanhan           ###   ########.fr       */
+/*   Updated: 2020/04/12 22:17:51 by sanhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!dst && !src)
+		return (0);
 	while (i < n)
 	{
 		((char *)dst)[i] = ((char *)src)[i];
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: sanhan <sanhan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 11:33:07 by sanhan            #+#    #+#             */
-/*   Updated: 2020/04/06 09:15:06 by sanhan           ###   ########.fr       */
+/*   Updated: 2020/04/12 22:55:37 by sanhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	temp[len];
-
-	ft_memcpy(temp, src, len);
-	ft_memcpy(dst, temp, len);
+	if (!dst && !src)
+		return (0);
+	if ((long long)dst = (long long)src)
+		return (dst);
+	else if ((long long)dst < (long long)src)
+		ft_memcpy(dst, src, len);
+	else
+		ft_rev_memcpy(dst, src, len);
 	return (dst);
 }
