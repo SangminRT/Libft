0aef9add32e566809b53a1ddc7621a0977abc80b
한상민 <javis@hansangmins-Mac.local>
2020-04-16 11:38:10 +0900

    edit ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: sanhan <sanhan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 17:04:22 by sanhan            #+#    #+#             */
-/*   Updated: 2020/04/13 12:19:01 by sanhan           ###   ########.fr       */
+/*   Updated: 2020/04/16 11:36:53 by sanhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,10 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	while (*lst != 0)
 	{
 		temp = *lst;
-		ft_lstdelone(temp, del);
+		del(temp->content);
 		*lst = (*lst)->next;
+		free(temp);
 	}	
 	*lst = 0;
 	return ;
 }
-/*
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	t_list	*temp;
-
-	while (*lst)
-	{
-		temp = *lst;
-		while (temp->next)
-			temp = temp->next;
-		del(temp->content);
-		free(temp);
-	}
-	*lst = 0;
-	return ;
-}
-*/
